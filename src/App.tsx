55c6d93b2a146ebeffa8d875c86072ee3c09dee4
ambiti0n <fabulousfresh010@gmail.com>
2022-06-06 07:54:53 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const initialCityState = { city: "", state: "" };
+  const [cityState, setCityState] = useState(initialCityState);
+  const [zipcode, setZipcode] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>City/State Lookup Tool</h1>
+      <form action="" className="form-data">
+        <label htmlFor="zip">Type Zip Code Here</label>
+        <input
+          className="zip"
+          value={zipcode}
+          placeholder="XXXXX"
+          type="text"
+          name="zip"
+          id="zip"
+          onChange={(event) => {
+            const { value } = event.target;
+            setZipcode(value.replace(/[^\d{5}]$/, "").substr(0, 5));
+          }}
+        />
+        <label htmlFor="city">City</label>
+        <input
+          className={`city`}
+          value={cityState.city}
+          type="text"
+          name="city"
+          disabled
+          id="city"
+        />
+        <label htmlFor="state">State</label>
+        <input
+          className={`state`}
+          value={cityState.state}
+          type="text"
+          name="state"
+          disabled
+          id="state"
+        />
+      </form>
+      <pre>
+        <code>
+          {JSON.stringify({
+            zipcode: zipcode,
+            city: cityState.city,
+            state: cityState.state,
+          })}
+        </code>
+      </pre>
     </div>
   );
 }
